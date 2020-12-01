71cd9b1d16f6868c546f5c29a84e230163007328
Vamsi Sannala <vamsireddysannala@gmail.com>
2020-12-01 12:12:52 +0530

    Add files via upload

---
+++ actions/actions.py
@@ -0,0 +1,59 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+class ActionCoronaState(Action):
+
+     def name(self) -> Text:
+         return "action_corona_state"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+         response = requests.get("https://api.covid19india.org/data.json").json()
+         entities = tracker.latest_message['entities']
+         print('Last Message Now',entities)
+         state = None
+
+         for e in entities:
+             if e['entity']=="state":
+                 state = e['value']
+
+         message = "Please enter correct state name"
+
+         for data in response["statewise"]:
+             if data["state"]== state.title():
+                 print(data)
+                 message = "Active: "+data['active'] + " ,Confirmed: "+data["confirmed"] + " ,Recovered: "+data["recovered"] +" ,last updated on: "+data["lastupdatedtime"]
+         
+
+         print(message)
+         dispatcher.utter_message(text=message)
+
+         return []
